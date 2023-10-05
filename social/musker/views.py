3b09243aedf9c4832d631e2bb5e4b3b10ffaefee
timmyternerrr <timmyterner47@gmail.com>
2023-10-04 19:19:27 -0700

    second commit

---
+++ social/musker/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import Profile, Meep
+
+# Create your views here.
+def home(request):
+    if request.user.is_authenticated:
+        meeps = Meep.objects.all()
+    return render(request, 'home.html', {"meeps":meeps})
+
+
+def profile_list(request):
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html',{"profiles":profiles})
+    else:
+        messages.success(request, ("You Must Be Logged In"))
+        return redirect('home')
+    
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        return render(request, "profile.html", {"profile":profile})
+    else:
+        messages.success(request, ("You Must Be Logged In"))
+        return redirect('home')     
